09d162a6fb7d3416855fc904ea2a6f7f57277de4
black13 <jjosburn@jjosburns-MBP.attlocal.net>
2016-10-23 09:26:30 -0500

    add to main

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/black13/gofun/cassandra/cassandra"
+)
+
+func main() {
+	xs := []float64{98, 93, 77, 82, 83}
+
+	cassandra.Add("dr", "Dart")
+	fmt.Println(cassandra.Get("dr"))
+	languages := cassandra.GetAll()
+	for _, v := range languages {
+		fmt.Println(v)
+	}
+	fmt.Println(cassandra.Average(xs))
+}
