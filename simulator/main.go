5208f12eaf75ecd6676821d3e7e04209cf5262c6
James Osburn <james.osburn@capitalone.com>
2016-11-30 15:33:04 -0600

    remove comments

--- simulator/main.go
+++ simulator/main.go
@@ -30,16 +30,6 @@ func NewSimulator(port int) *simulator {
 }
 
 func main() {
-	/*
-	server, err := net.Listen("tcp", ":"+strconv.Itoa(PORT))
-	if server == nil {
-		panic(fmt.Sprintf("echo: failed to listen on %v: %v", server, err))
-	}
-	conns := clientConns(server)
-	for {
-		go handleConn(<-conns)
-	}
-	*/
 	sim := NewSimulator(3540)
 	sim.connect()
 
@@ -99,35 +89,3 @@ func (v *simulator) clientConns(listener net.Listener) chan net.Conn {
 	func (v *simulator) done() interface{} {
 		return "done"
 	}
-
-/*
-func clientConns(listener net.Listener) chan net.Conn {
-	ch := make(chan net.Conn)
-	i := 0
-	go func() {
-		for {
-			client, err := listener.Accept()
-			if client == nil {
-				//fmt.Printf("couldn't accept: " + err.String())
-				fmt.Printf("echo: failed to listen on %v: %v", client, err)
-				continue
-			}
-			i++
-			fmt.Printf("%d: %v <-> %v\n", i, client.LocalAddr(), client.RemoteAddr())
-			ch <- client
-		}
-	}()
-	return ch
-}
-
-func handleConn(client net.Conn) {
-	b := bufio.NewReader(client)
-	for {
-		line, err := b.ReadBytes('\n')
-		if err != nil { // EOF, or worse
-			break
-		}
-		client.Write(line)
-	}
-}
-*/
